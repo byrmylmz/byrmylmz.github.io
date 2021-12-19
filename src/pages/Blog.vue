3eb71f2bed5c2a914e21017c0cb7856f74a9e77c
Bayram Yılmaz <68708205+byrmylmz@users.noreply.github.com>
2021-12-19 12:25:44 +0300

    Update Blog.vue

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -2,17 +2,49 @@
   <Layout>
     <div class="container mx-auto py-16 ">
       <div class="">
-       
+        <!-- TOTAL NUMBER OF -->
+       <!-- <span>total article {{$page.posts.totalCount}}</span>
+       <span>total article {{$page.posts.totalCount}}</span> -->
+
           <div class="grid grid-cols-4 gap-4 mb-12">
-            <div v-for="post in $page.posts.edges" :key="post.id" class="post border-gray-400 border-b rounded-lg border p-4 hover:border-gray-500 cursor-pointer">
-              <h2 class=" font-bold truncate"><g-link :to="post.node.path" class="text-copy-primary">{{ post.node.title }}</g-link></h2>
-              <div class="text-copy-secondary mb-4">
-                <span>{{ post.node.date }}</span>
-                <span> &middot; </span>
-                <span>{{ post.node.timeToRead }} min read</span>
-                <span>{{ post.node.tags.id }}</span>
-              </div>
-            </div>
+            
+                  <g-link  v-for="post in $page.posts.edges" :key="post.id" :to="post.node.path" class= "
+                  post 
+                  shadow-lg  
+                  border-gray-300 
+                  border-2 
+                  rounded-lg 
+                  p-3 
+                  hover:border-gray-400 
+                  cursor-pointer 
+                  
+                  ">
+                    <div class=" font-semibold truncate text-md text-gray-800">{{ post.node.title }}</div>
+                    <div class="flex items-center space-x-2">
+                      <div v-for="tag in post.node.tags" :key="tag.id" class="flex flex-wrap " >
+                         
+                          <g-link 
+                            :to="tag.path"
+                             class="bg-blue-200
+                                     rounded-full
+                                     px-1 
+                                     hover:bg-blue-500 
+                                     text-xs
+                                     text-blue-800
+                                     hover:text-blue-100
+                                     shadow-xs 
+                                     font-semibold
+                                     
+                                     "
+                          >
+                          {{tag.title}}
+                          </g-link>
+                      </div>
+                      
+                    </div>
+                  
+                  </g-link >
+            
           </div>
       
       </div>
@@ -23,7 +55,12 @@
         :totalPages="$page.posts.pageInfo.totalPages"
         :currentPage="$page.posts.pageInfo.currentPage"
       />
-    </div>
+
+        <!-- TEST CODE FOR THE DENEME QUERY  -->
+          <!-- <div v-for="edge in $page.deneme.edges" :key="edge.node.id">
+            <h2>{{ edge.node.tags }}</h2>
+          </div>-->
+  </div> 
   </Layout>
 </template>
 
@@ -37,20 +74,43 @@ query Posts ($page: Int) {
     }
     edges {
       node {
+        tags{
+          id
+          title
+          path
+        }
         id
         title
         date (format: "MMMM D, Y")
         summary
-        tags{
-          id
-          title
-        }
         timeToRead
         path
       }
     }
   }
+
+ deneme:allPost{
+    edges{
+      node{
+        id
+        date
+        title
+        tags{
+          id
+          title
+        }
+      }
+     
+    }
+  
 }
+
+
+}
+
+
+ 
+
 </page-query>
 
 <script>
