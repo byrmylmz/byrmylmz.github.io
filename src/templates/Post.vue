982ac9df46d67016d1f3de078427f3ee450162f4
Bayram Yılmaz <68708205+byrmylmz@users.noreply.github.com>
2021-12-19 14:35:29 +0300

    Update Post.vue

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -8,7 +8,7 @@
           :to="tag.path"
           v-for="tag in $page.post.tags"
           :key="tag.id"
-          class="bg-gray-300 rounded-full px-4 py-2 mr-4 hover:bg-green-300">
+          class="bg-gray-300 rounded-full px-4 py-2 mr-4 hover:bg-blue-300">
           {{ tag.title }}
         </g-link>
       </div>
@@ -16,6 +16,20 @@
       <div class="mb-8">
         <g-link to="/blog" class="font-bold uppercase">Back to Blog</g-link>
       </div>
+      <a :href="'https://github.com/byrmylmz/byrmylmz.github.io/edit/master/'+$page.post.fileInfo.path"
+      class="
+      text-sm
+      ">
+      <!-- Edit icon -->
+        <div class="flex items-center space-x-2">
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+            </svg>
+          </div>
+          <div>Edit This Page on Github.</div>
+        </div>
+      </a>
     </div>
   </Layout>
 </template>
@@ -26,6 +40,13 @@ query Post ($path: String!) {
     title
     date (format: "MMMM D, Y")
     content
+    fileInfo{
+          extension
+          directory
+          path
+          name
+          
+        }
     tags {
       title
       path
